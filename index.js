babf3b602f2e9ae614cf23d64f608cb2eb4310cf
caitlyn-vanhaverbeke <cvdennis318@yahoo.com>
2021-07-19 14:42:43 -0500

    task 3 done

--- index.js
+++ index.js
@@ -81,9 +81,19 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(scoreCb, numberOfInnings){
+  const currentScore = {
+    'Home': 0,
+    'Away': 0
+  };
+  for (let i=0; i<=numberOfInnings; i++){
+    currentScore['Home'] += scoreCb();
+    currentScore['Away'] += scoreCb();
+  }
+  return currentScore;
 }
+
+console.log(finalScore(inning, 9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
